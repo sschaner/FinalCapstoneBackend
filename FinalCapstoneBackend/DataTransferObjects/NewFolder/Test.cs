0e59ed6c338da92dc508b2509f2d62124c609710
Steve Schaner <schanerst@gmail.com>
2022-03-31 20:13:00 -0400

    Added a test class.

---
+++ FinalCapstoneBackend/DataTransferObjects/NewFolder/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalCapstoneBackend.DataTransferObjects.NewFolder
+{
+    public class Test
+    {
+        public int MyProperty { get; set; }
+    }
+}
