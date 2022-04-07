0c7c9067902a8132778360331ea1a6fe4bf3e0d4
Zachary Tata <86603180+ztata@users.noreply.github.com>
2022-04-06 20:24:31 -0400

    added cors workaround to startup.cs

--- FinalCapstoneBackend/Startup.cs
+++ FinalCapstoneBackend/Startup.cs
@@ -26,6 +26,21 @@ namespace FinalCapstoneBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+
+            {
+
+                options.AddPolicy(
+
+                name: "AllowOrigin",
+
+                builder => {
+
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+
+                });
+
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sunny Trails", Version = "v1", });
@@ -36,6 +51,7 @@ namespace FinalCapstoneBackend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("AllowOrigin");
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
